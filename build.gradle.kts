5fb8e80000b4122e6416cbe960a4bf880f3074f2
박세영 <psy@sellmate.co.kr>
2024-09-06 21:52:16 +0900

    setting: 의존성 및 actions, listener 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,14 @@ repositories {
     }
 }
 
+repositories {
+    mavenCentral()
+}
+
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
     testImplementation(libs.junit)
+    implementation ("com.google.code.gson:gson:2.8.8")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
