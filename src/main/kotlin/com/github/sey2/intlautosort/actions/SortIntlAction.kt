62d88d274be8c739dd5bec6f932f13037e5b6b68
박세영 <psy@sellmate.co.kr>
2024-09-08 20:54:22 +0900

    feat: 디렉토리 단위 정렬 기능 추가

--- src/main/kotlin/com/github/sey2/intlautosort/actions/SortIntlAction.kt
+++ src/main/kotlin/com/github/sey2/intlautosort/actions/SortIntlAction.kt
@@ -2,8 +2,8 @@ package com.github.sey2.intlautosort.actions
 
 import com.github.sey2.intlautosort.domain.json.JsonFixer
 import com.github.sey2.intlautosort.domain.json.JsonSorter
-import com.github.sey2.intlautosort.enums.FileExtensionType
 import com.github.sey2.intlautosort.domain.usecase.SortJsonFileUseCase
+import com.github.sey2.intlautosort.enums.FileExtensionType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -19,13 +19,25 @@ class SortIntlAction(
         JsonFixer(),
     )
 ) : AnAction() {
+
     override fun actionPerformed(e: AnActionEvent) {
         val project: Project = e.project ?: return
-        val virtualFile: VirtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
 
-        when (virtualFile.extension) {
+        e.getData(CommonDataKeys.VIRTUAL_FILE)?.run {
+            if (this.isDirectory) {
+                this.children.forEach { childFile ->
+                    handleToSortFiles(project, childFile)
+                }
+            } else {
+                handleToSortFiles(project, this)
+            }
+        }
+    }
+
+    private fun handleToSortFiles(project: Project, file: VirtualFile) {
+        when (file.extension) {
             FileExtensionType.arb.name, FileExtensionType.json.name -> {
-                sortJsonFileUseCase.execute(project, virtualFile)
+                sortJsonFileUseCase.execute(project, file)
                     .onFailure { ex ->
                         NotificationService.showErrorNotification(
                             project,
