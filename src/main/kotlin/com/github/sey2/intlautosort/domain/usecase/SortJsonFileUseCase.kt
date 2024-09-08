94280ee44f76f2b62d00ee026ae94f50cf2d0389
박세영 <psy@sellmate.co.kr>
2024-09-08 20:23:44 +0900

    feat: command + option + s 입력시 자동 정렬 기능 추가

---
+++ src/main/kotlin/com/github/sey2/intlautosort/domain/usecase/SortJsonFileUseCase.kt
@@ -0,0 +1,67 @@
+package com.github.sey2.intlautosort.domain.usecase
+
+import com.github.sey2.intlautosort.domain.json.JsonFixer
+import com.github.sey2.intlautosort.domain.json.JsonSorter
+import com.google.gson.stream.MalformedJsonException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.github.sey2.intlautosort.infra.FileService
+import com.github.sey2.intlautosort.utils.NotificationService
+
+class SortJsonFileUseCase(
+    private val jsonSorter: JsonSorter,
+    private val fileService: FileService,
+    private val jsonFixer: JsonFixer,
+) {
+    fun execute(project: Project, file: VirtualFile) = runCatching {
+        fileService.readFile(file).apply {
+            jsonSorter.sortJson(this).apply {
+                fileService.writeFile(project, file, this)
+            }
+        }
+    }.onFailure { ex ->
+        when (ex) {
+            is MalformedJsonException -> {
+                handleJsonSyntaxError(project, file, ex)
+            }
+
+            else -> {
+                throw ex
+            }
+        }
+    }
+
+    private fun handleJsonSyntaxError(project: Project, file: VirtualFile, ex: MalformedJsonException) {
+        NotificationService.showErrorNotification(
+            project,
+            "JSON Syntax Error",
+            "There is a syntax error in the JSON file: ${ex.message}"
+        )
+
+        jsonFixer.fixMalformedJson(fileService.readFile(file)).run {
+            onSuccess { content ->
+                tryToSortFixedJson(project, file, content)
+            }.onFailure {
+                NotificationService.showErrorNotification(
+                    project,
+                    "Fix Unsuccessful",
+                    "Unable to automatically fix the JSON syntax."
+                )
+            }
+        }
+    }
+
+    private fun tryToSortFixedJson(project: Project, file: VirtualFile, fixedContent: String) {
+        runCatching {
+            jsonSorter.sortJson(fixedContent).run {
+                fileService.writeFile(project, file, this)
+            }
+        }.onFailure { ex ->
+            NotificationService.showErrorNotification(
+                project,
+                "Fix Failed",
+                "Failed to fix the JSON file: ${ex.message}"
+            )
+        }
+    }
+}
